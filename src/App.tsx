29682321f0efa5903c76e3991ef3abdeb67f25df
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-08-12 00:51:29 +0200

    feat(theme): implement theme toggle functionality in App component (W.I.P)

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,14 @@ import {ComicButton} from "./component/ComicButton.tsx";
 import {useEffect, useState} from "react";
 import {AboutSection} from "./component/sections/AboutSection.tsx";
 import {ContactSection} from "./component/sections/ContactSection.tsx";
+import {retrieveThemeStatus} from "./utils/themeHandling.ts";
+
 
 function App() {
     const [scrollEl, setScrollEl] = useState<HTMLDivElement | null>(null);
     const [activeSection, setActiveSection] = useState("home");
+
+    const [isBlackTheme, setBlackTheme] = useState<boolean>(retrieveThemeStatus);
 
     useEffect(() => {
         if (!scrollEl) return;
@@ -21,7 +25,7 @@ function App() {
                     setActiveSection(visible[0].target.id);
                 }
             },
-            { root: scrollEl, threshold: 0.2 }
+            {root: scrollEl, threshold: 0.2}
         );
 
         ["home", "about", "projects", "contact"].forEach((id) => {
@@ -54,20 +58,20 @@ function App() {
         return () => observer.disconnect();
     }, [scrollEl]);
 
-    return <Scrollbar ref={setScrollEl}>
+    return <Scrollbar ref={setScrollEl} theme={isBlackTheme}>
         <div className="relative h-screen">
             <div
                 className={`fixed inset-0 z-1 w-screen h-screen pointer-events-none backdrop-blur-2xl transition-opacity duration-500 ${activeSection == "home" ? "opacity-0" : "opacity-100"}`}></div>
             <div className="fixed inset-0 z-0 w-screen h-screen pointer-events-none">
                 <Color4Bg style={"curve-gradient"}
-                          colors={["#5c5c5c", "#808080", "#b0b0b0", "#e6e6e6", "#EAEBE8", "#8f8f8f"]}
+                          colors={isBlackTheme ? ["#FDFDFD","#DDDDDD","#BBBBBB","#555555","#343434","#010101"] : ["#5c5c5c", "#808080", "#b0b0b0", "#e6e6e6", "#EAEBE8", "#8f8f8f"]}
                           loop={true}
                           seed={1000}
                           options={{speed: 1, noise: 0.15}}/>
             </div>
             <div className={"flex flex-col gap-70"}>
                 <div
-                    className="w-full h-screen z-10 relative cursor-default select-none p-10 flex flex-col justify-end items-start"
+                    className="w-full h-screen z-10 relative cursor-default select-none p-10 flex flex-col justify-center xl:justify-end items-start"
                     id={"home"}>
                     <div className={"flex flex-row"}>
                         <h1 className="font-[Panchang-Variable] font-semibold text-9xl hover:font-black transition-all">N</h1>
@@ -75,16 +79,23 @@ function App() {
                         <h1 className="font-[Panchang-Variable] font-semibold text-9xl hover:font-black transition-all">a</h1>
                         <h1 className="font-[Panchang-Variable] font-semibold text-9xl hover:font-black transition-all">h</h1>
                     </div>
-                    <div className="flex flex-row justify-start items-center">
-                        <h1 className="font-[Panchang-Variable] font-semibold text-[7rem] hover:font-black transition-all duration-300">CHARRIN</h1>
+                    {/*name for big screens*/}
+                    <div className="hidden flex-row justify-start items-center 2xl:flex">
+                        <h1 className="font-[Panchang-Variable] font-semibold text-[6.3rem] hover:font-black transition-all duration-300">CHARRIN</h1>
                         <div className={"flex flex-row"}>
                             <h1 className="font-[Panchang-Variable] font-semibold text-8xl">-</h1>
                             <h1 className="font-[Panchang-Variable] font-semibold text-8xl">-</h1></div>
-                        <h1 className="font-[Panchang-Variable] font-semibold text-[7rem] hover:font-black transition-all duration-300">BOURRAT</h1>
+                        <h1 className="font-[Panchang-Variable] font-semibold text-[6.3rem] hover:font-black transition-all duration-300">BOURRAT</h1>
                     </div>
-                    <div className={"w-full h-30 flex flex-row justify-between items-center"}>
+                    {/*name for smaller screens*/}
+                    <div className="flex flex-col justify-center items-start 2xl:hidden">
+                        <h1 className="font-[Panchang-Variable] font-semibold text-[6.5rem] hover:font-black transition-all duration-300">CHARRIN--</h1>
+                        <h1 className="font-[Panchang-Variable] font-semibold text-[6.5rem] hover:font-black transition-all duration-300">BOURRAT</h1>
+                    </div>
+                    <div
+                        className={"w-full h-30 flex 2xl:flex-row flex-col 2xl:justify-between 2xl:items-center items-start"}>
                         <h2 className="font-[DINdong] font-black text-[4rem]">Etudiant en informatique</h2>
-                        <div className={"flex flex-row justify-center items-center gap-5"}>
+                        <div className={"xl:w-auto w-full flex flex-row justify-center items-center gap-5"}>
                             <ComicButton text="CV"
                                          link="https://drive.google.com/file/d/1G76TPsTcdBMbECQ9myqVTopUqWO0IgC-/view?usp=sharing"/>
                             <ComicButton text="GitHub" link="https://github.com/cbnoah"/>
