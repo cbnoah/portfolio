6a74078664fcc276b15b90c0a6d4f742b4970b74
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-08-21 01:10:14 +0200

    feat(StepComponent): enhance styling and add markdown support for description

--- src/component/StepComponent.tsx
+++ src/component/StepComponent.tsx
@@ -1,5 +1,6 @@
 import {Building2, MapPin, Maximize2, Minimize2} from "lucide-react";
 import {useState} from "react";
+import {DescriptionMarkdown} from "./DescriptionMarkdown.tsx";
 
 type StepComponenentProps = {
     title: string,
@@ -8,7 +9,7 @@ type StepComponenentProps = {
     country: string,
     beginningDate: Date,
     endingDate?: Date,
-    description: string
+    description: string,
     showExpandBtn?: boolean
 }
 
@@ -27,29 +28,34 @@ export function StepComponent({
     let [height] = useState(showExpandBtn == true && showExpandBtn !== null ? "h-160" : "h-150");
     return (
         <div
-            className={`bg-gray-200/45 ${description.length > 1000 ? (expanded ? "h-full" : height) : "h-full"} w-full rounded-3xl flex flex-col items-center justify-start py-10 px-15 gap-8 border border-white/30 hover:border-white hover:bg-gray-200 transition-all duration-300 `}>
+            className={`dark:bg-[#09090B]/45 bg-gray-200/45 ${description.length > 1000 ? (expanded ? "h-full" : height) : "h-full"} w-full rounded-3xl flex flex-col items-center justify-start py-10 px-15 gap-8 border border-white/30 dark:border-dark/30 hover:border-white dark:hover:border-black hover:bg-gray-200 dark:hover:bg-[#09090B]/70 transition-all duration-300`}>
             {/*Component header (for infos)*/}
             <div className={"w-full"}>
-                <h3 className={"text-4xl font-extrabold mb-2 font-[Anybody] w-full"}>{title}</h3>
-                <h4 className={"text-md font-medium mb-2 jetbrains-mono w-50 text-center bg-gray-100/80 rounded-xl"}>{beginningDate.getMonth() + "/" + beginningDate.getFullYear()} - {endingDate ? endingDate.getMonth() + 1 + "/" + endingDate.getFullYear() : 'Present'}</h4>
+                <h3 className={"text-4xl font-extrabold mb-2 font-[Anybody] text-black dark:text-white"}>{title}</h3>
+                <h4 className={"text-md font-medium mb-2 jetbrains-mono w-50 text-center bg-gray-100/80 dark:bg-black/80 rounded-xl text-black dark:text-white"}>{beginningDate.getMonth() + "/" + beginningDate.getFullYear()} - {endingDate ? endingDate.getMonth() + 1 + "/" + endingDate.getFullYear() : 'Present'}</h4>
                 <div className={"w-full flex flex-row justify-between items-center"}>
                     <div className={"flex flex-row gap-2"}>
-                        <Building2/>
-                        <h4 className={"text-lg mb-2 jetbrains-mono"}>{company}</h4>
+                        <Building2 className={"text-black dark:text-white"}/>
+                        <h4 className={"text-lg mb-2 jetbrains-mono text-black dark:text-white"}>{company}</h4>
                     </div>
                     <div className={"flex flex-row gap-2"}>
-                        <MapPin/>
-                        <h4 className={"text-lg mb-2 jetbrains-mono"}>{city}, {country}</h4>
+                        <MapPin className={"text-black dark:text-white"}/>
+                        <h4 className={"text-lg mb-2 jetbrains-mono text-black dark:text-white"}>{city}, {country}</h4>
                     </div>
                 </div>
             </div>
             {/*Text Part*/}
             <div className={"w-full h-full"}>
-                <p className={`${expanded ? '' : 'line-clamp-13 xl:line-clamp-13 2xl:line-clamp-15'}`}>{description}</p>
+                <div
+                    className={`${expanded ? '' : 'line-clamp-9'} markdown-content`}>
+                    <DescriptionMarkdown description={description}/>
+                </div>
             </div>
             <>{(showExpandBtn === true && showExpandBtn !== null) &&
                 <button
-                    className={"w-full flex flex-row gap-3 items-center justify-center jetbrains-mono font-bold bg-gray-100/60 border border-white/30 py-3 rounded-3xl hover:border-white hover:bg-gray-100 hover:scale-102 transition-all duration-300"}
+                    className={"w-full flex flex-row gap-3 items-center justify-center jetbrains-mono font-bold bg-gray-100/60 " +
+                        "dark:bg-[#09090B]/60 border border-white/30 py-3 rounded-3xl hover:border-white hover:bg-gray-100 hover:scale-102 " +
+                        "dark:hover:bg-[#09090B] transition-all duration-300 text-black dark:text-white"}
                     onClick={() => {
                         setExpanded(!expanded)
                     }}>
