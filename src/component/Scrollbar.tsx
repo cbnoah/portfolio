96f5efbcb63258361b5107e16c50e1380ec40afa
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-08-21 01:10:06 +0200

    feat(Scrollbar): improve styling and responsiveness for dark mode support

--- src/component/Scrollbar.tsx
+++ src/component/Scrollbar.tsx
@@ -167,23 +167,23 @@ export const Scrollbar = forwardRef<HTMLDivElement, { children: React.ReactNode;
                 </div>
                 <div className={"flex flex-col items-center justify-center"}>
                     <div
-                        className={`absolute h-25 bg-gray-50/40 z-100 transition-all duration-200 flex flex-row
-                    ${smallTopBar ? "w-full md:w-6xl top-10 rounded-3xl py-6 px-10 drop-shadow-xl drop-shadow-black/30 backdrop-blur-2xl" : "w-full rounded-b-3xl top-0 right-0 py-4 px-1.5 xl:px-15 backdrop-blur-xl drop-shadow-black/30"}`}>
+                        className={`absolute h-25 bg-gray-50/40 dark:bg-[#2a2b2b]/40 z-100 transition-all duration-200 flex flex-row
+                    ${smallTopBar ? "w-4xl xl:w-6xl top-10 rounded-3xl py-6 px-10 drop-shadow-xl drop-shadow-black/30 backdrop-blur-2xl" : "w-full rounded-b-3xl top-0 right-0 py-4 px-1.5 xl:px-15 backdrop-blur-xl drop-shadow-black/30"}`}>
                         <div className={"h-full min-w-full flex flex-row justify-between items-center gap-5"}>
                             <div
-                                className={`w-full h-full flex flex-row justify-between items-center ${smallTopBar ? "xl:gap-25 gap-5" : "xl:gap-30 gap-5"}`}>
+                                className={`w-full h-full flex flex-row justify-between items-center ${smallTopBar ? "px-0" : "md:px-15 2xl:px-75"}`}>
                                 <a href={"#home"}
-                                   className={`text-black text-2xl xl:text-3xl font-[Anybody] p-4 xl:p-5 rounded-3xl transition-all duration-500 ${activeSection == "home" ? "shadow-(--inset-top-bar-button-shadow) shadow-[#e0e0e0] font-medium" : "font-light"}`}>Accueil</a>
+                                   className={`text-black dark:text-gray-200 text-2xl xl:text-3xl font-[Anybody] p-4 xl:p-5 rounded-3xl transition-all duration-500 ${activeSection == "home" ? "shadow-(--inset-top-bar-button-shadow) shadow-[#e0e0e0] dark:shadow-[#3f3f3f] font-medium" : "font-light"}`}>Accueil</a>
                                 <a href={"#about"}
-                                   className={`text-black text-2xl xl:text-3xl font-[Anybody]  p-4 xl:p-5 rounded-3xl transition-all duration-500 ${activeSection == "about" ? "shadow-(--inset-top-bar-button-shadow) shadow-[#e0e0e0] font-medium" : "font-light"}`}>Parcours</a>
+                                   className={`text-black dark:text-gray-200 text-2xl xl:text-3xl font-[Anybody]  p-4 xl:p-5 rounded-3xl transition-all duration-500 ${activeSection == "about" ? "shadow-(--inset-top-bar-button-shadow) shadow-[#e0e0e0] dark:shadow-[#3f3f3f] font-medium" : "font-light"}`}>Parcours</a>
                                 <a href={"#projects"}
-                                   className={`text-black text-2xl xl:text-3xl font-[Anybody] p-4 xl:p-5 rounded-3xl transition-all duration-500 ${activeSection == "projects" ? "shadow-(--inset-top-bar-button-shadow) shadow-[#e0e0e0] font-medium" : "font-light"}`}>Projects</a>
+                                   className={`text-black dark:text-gray-200 text-2xl xl:text-3xl font-[Anybody] p-4 xl:p-5 rounded-3xl transition-all duration-500 ${activeSection == "projects" ? "shadow-(--inset-top-bar-button-shadow) shadow-[#e0e0e0] dark:shadow-[#3f3f3f] font-medium" : "font-light"}`}>Projects</a>
                                 <a href={"#contact"}
-                                   className={`text-black text-2xl xl:text-3xl font-[Anybody] p-4 xl:p-5 rounded-3xl transition-all duration-500 ${activeSection == "contact" ? "shadow-(--inset-top-bar-button-shadow) shadow-[#e0e0e0] font-medium" : "font-light"}`}>Contact</a>
+                                   className={`text-black dark:text-gray-200 text-2xl xl:text-3xl font-[Anybody] p-4 xl:p-5 rounded-3xl transition-all duration-500 ${activeSection == "contact" ? "shadow-(--inset-top-bar-button-shadow) shadow-[#e0e0e0] dark:shadow-[#3f3f3f] font-medium" : "font-light"}`}>Contact</a>
                             </div>
                             <div className={"h-full flex flex-row justify-center items-center gap-6 xl:gap-7"}>
                                 <div className={"h-full w-0.5 backdrop-invert-100"}></div>
-                                <div className={"w-full xl:w-auto h-full flex md:flex-row flex-col justify-around xl:justify-center items-center md:gap-5 pr-3 xl:pr-0"}>
+                                <div className={"w-full xl:w-auto h-full flex md:flex-row flex-col justify-around xl:justify-center items-center md:gap-5 pr-3 xl:pr-0 text-black dark:text-gray-200"}>
                                     <Languages size={30} strokeWidth={1.5}/>
                                     <ThemeToggle/>
                                 </div>
@@ -196,11 +196,11 @@ export const Scrollbar = forwardRef<HTMLDivElement, { children: React.ReactNode;
                              role={"scrollbar"}
                              className={"h-full relative w-4 pointer-events-auto flex justify-center"}>
                             <div
-                                className={"hidden xl:grid bottom-0 top-0 cursor-pointer absolute w-2 hover:w-full h-full bg-gray-600/50 hover:backdrop-invert-75 rounded-3xl pointer-events-auto transition-all"}
+                                className={"hidden md:grid bottom-0 top-0 cursor-pointer absolute w-2 hover:w-full h-full bg-gray-500/80 hover:backdrop-invert-75 rounded-3xl pointer-events-auto transition-all"}
                                 ref={scrollTrackRef}
                                 style={{cursor: isDragging ? "grabbing" : undefined}}
                                 onClick={handleTrackClick}></div>
-                            <div className={"hidden xl:grid absolute w-full bg-[#262626] rounded-3xl transition-colors"}
+                            <div className={"hidden md:grid absolute w-full bg-[#262626] dark:bg-[#bebebe] rounded-3xl transition-colors"}
                                  ref={scrollThumbRef}
                                  onMouseDown={handleThumbMousedown}
                                  style={{
