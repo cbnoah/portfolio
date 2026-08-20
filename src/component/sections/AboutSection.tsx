ac6174a1f2660046027adc6ff3962e047f6dd887
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-08-21 01:10:43 +0200

    feat(AboutSection): update description with detailed internship experience and enhance section styling

--- src/component/sections/AboutSection.tsx
+++ src/component/sections/AboutSection.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {StepComponent} from "../StepComponenent.tsx";
+import {StepComponent} from "../StepComponent.tsx";
 
 export function AboutSection() {
     const carreerPath = [
@@ -10,54 +10,58 @@ export function AboutSection() {
             country={"France"}
             beginningDate={new Date(2026, 1)}
             endingDate={new Date(2026, 5)}
-            description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent\n" +
-                "                    malesuada, metus at venenatis vehicula, enim orci cursus orci, sed pharetra nisi eros eget tortor.\n" +
-                "                    Integer faucibus facilisis purus vel faucibus. Vivamus ac arcu eu risus tincidunt vehicula eu id lorem.\n" +
-                "                    Morbi quis erat sodales, blandit libero eget, lacinia ligula. Nunc sit amet mollis mauris, sit amet\n" +
-                "                    accumsan ipsum. In hac habitasse platea dictumst. In faucibus leo nec diam rutrum maximus. Morbi eu\n" +
-                "                    ligula faucibus turpis ornare mollis. Praesent elementum a felis vel hendrerit. Praesent rhoncus a dui\n" +
-                "                    quis viverra. Sed vulputate dictum neque, eget sodales tortor sagittis vitae. Vivamus iaculis diam sit\n" +
-                "                    amet mi suscipit fermentum. Nullam malesuada ex rhoncus nisi mollis ullamcorper. Aliquam fringilla vel\n" +
-                "                    eros ac mattis. Nulla facilisi. In nibh mauris, pellentesque id finibus vehicula, euismod vitae magna.\n" +
-                "                    Nunc vel nulla sapien. In hendrerit lacinia mauris sit amet ornare. Nunc faucibus auctor est id gravida.\n" +
-                "                    Aenean suscipit, dolor nec tempor fermentum, nisi purus rutrum massa, aliquet venenatis tellus tortor\n" +
-                "                    eget nibh. Maecenas convallis suscipit leo id finibus. Quisque semper mi est, suscipit faucibus sapien\n" +
-                "                    accumsan sit amet. Pellentesque suscipit libero lacinia fermentum lobortis. Etiam euismod, leo elementum\n" +
-                "                    efficitur auctor, velit arcu laoreet nisi, ac euismod mi magna sed dolor. Vestibulum ante ipsum primis\n" +
-                "                    in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse sit amet lectus at ex imperdiet\n" +
-                "                    pellentesque. Integer sed porta ex, id fringilla metus. Donec luctus posuere tempus. Ut pellentesque\n" +
-                "                    euismod arcu, elementum tempus arcu sodales sed. Donec quis bibendum massa. Aenean id nisi leo.\n" +
-                "                    Pellentesque laoreet ex nec sem gravida rutrum. Etiam placerat nec dolor non aliquet. Suspendisse vel\n" +
-                "                    tincidunt erat. Sed bibendum fermentum imperdiet. Aenean ex sapien, interdum non dictum eget,\n" +
-                "                    sollicitudin at sapien. Sed fringilla dui est, non ornare ex pharetra eu. Donec turpis urna, convallis\n" +
-                "                    sit amet luctus sed, convallis in leo. Integer nisl purus, efficitur eu dui nec, pharetra pellentesque\n" +
-                "                    ex. Curabitur a dui purus. Praesent pellentesque fringilla enim id lacinia. Vestibulum pellentesque\n" +
-                "                    venenatis arcu nec blandit. Etiam fringilla sem faucibus sagittis consequat. Proin a nunc vitae nibh\n" +
-                "                    tristique bibendum sit amet vulputate nunc. Ut porttitor dolor id ornare rutrum. Vestibulum ut massa\n" +
-                "                    nisl. Nulla vitae eros sit amet metus imperdiet malesuada id sit amet lectus. Nam ac felis ipsum.\n" +
-                "                    Curabitur dui quam, volutpat a orci sit amet, tristique aliquet ex. Pellentesque ac dui nec sem placerat\n" +
-                "                    pulvinar. Phasellus lacinia maximus nisl, faucibus gravida lorem lacinia eget.Lorem ipsum dolor sit\n" +
-                "                    amet, consectetur adipiscing elit. Praesent malesuada, metus at venenatis vehicula, enim orci cursus\n" +
-                "                    orci, sed pharetra nisi eros eget tortor. Integer faucibus facilisis purus vel faucibus. Vivamus ac arcu\n" +
-                "                    eu risus tincidunt vehicula eu id lorem. Morbi quis erat sodales, blandit libero eget, lacinia ligula.\n" +
-                "                    Nunc sit amet mollis mauris, sit amet accumsan ipsum. In hac habitasse platea dictumst. In faucibus leo\n" +
-                "                    nec diam rutrum maximus. Morbi eu ligula faucibus turpis ornare mollis. Praesent elementum a felis vel\n" +
-                "                    hendrerit. Praesent rhoncus a dui quis viverra. Sed vulputate dictum neque, eget sodales tortor sagittis\n" +
-                "                    vitae. Vivamus iaculis diam sit amet mi suscipit fermentum. Nullam malesuada ex rhoncus nisi mollis\n" +
-                "                    ullamcorper. Aliquam fringilla vel eros ac mattis. Nulla facilisi. In nibh mauris, pellentesque id\n" +
-                "                    finibus vehicula, euismod vitae magna. Nunc vel nulla sapien. In hendrerit lacinia mauris sit amet\n" +
-                "                    ornare. Nunc faucibus auctor est id gravida. Aenean suscipit, dolor nec tempor fermentum, nisi purus\n" +
-                "                    rutrum massa, aliquet venenatis tellus tortor eget nibh. Maecenas convallis suscipit leo id finibus.\n" +
-                "                    Quisque semper mi est, suscipit faucibus sapien accumsan sit amet. Pellentesque suscipit libero lacinia\n" +
-                "                    fermentum lobortis. Etiam euismod, leo elementum efficitur auctor, velit arcu laoreet nisi, ac euismod\n" +
-                "                    mi magna sed dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia\n" +
-                "                    curae; Suspendisse sit amet lectus at ex imperdiet pellentesque. Integer sed porta ex, id fringilla\n" +
-                "                    metus. Donec luctus posuere tempus. Ut pellentesque euismod arcu, elementum tempus arcu sodales sed.\n" +
-                "                    Donec quis bibendum massa. Aenean id nisi leo. Pellentesque laoreet ex nec sem gravida rutrum. Etiam\n" +
-                "                    placerat nec dolor non aliquet. Suspendisse vel tincidunt erat. Sed bibendum fermentum imperdiet. Aenean\n" +
-                "                    ex sapien, interdum non dictum eget, sollicitudin at sapien. Sed fringilla dui est, non ornare ex\n" +
-                "                    pharetra eu."}
-        showExpandBtn={true}/>
+            description={`
+# L'entreprise et son marché:
+*Nom et localisation :* QGS Solutions (basée à Narbonne).
+
+*Secteur et positionnement :* Fondée en 2024, QGS Solutions est une entreprise qui accompagne les TPE, PME et indépendants dans la gestion de leur informatique et la sécurisation de leurs données. Elle se positionne sur le marché concurrentiel du service informatique et des prestations de proximité.
+
+*Cadre du stage :* Mon expérience en tant que stagiaire en développement full stack s'est déroulée en format alterné (travail pour l'entreprise les après-midis) de janvier à mai 2026, soit une durée totale de 5 mois.
+
+# Rôle et activités
+La tâche principale qui m'a été confiée était de concevoir, reproduire et moderniser un SaaS à destination des viticulteurs pour la gestion de leurs stocks et de leurs ventes : VinoDomia. 
+L'ancien logiciel possédait une interface vieillissante et présentait des limites de maintenance importantes, reposant sur des outils de développement obsolètes. 
+L'enjeu stratégique était de recoder entièrement le logiciel sous la forme d'une web app en utilisant des technologies modernes, afin d'offrir un accès simplifié depuis n'importe quel appareil connecté au réseau 
+et de supprimer la nécessité d'installer un programme lourd sur les machines des utilisateurs. Mes activités ont principalement porté sur la modélisation des interfaces, 
+l'architecture frontend et le début de l'implémentation backend.
+
+# Outils techniques et méthodes
+
+Pour la partie frontend, j'ai utilisé TypeScript combiné à Nuxt (un framework basé sur l'écosystème Vue.js), avec l'ajout de TailwindCSS pour faciliter la modélisation des interfaces. 
+Pour le backend, la structuration du schéma de la base de données et sa manipulation ont été réalisées via l'ORM Prisma.
+
+Lors du développement, des méthodess de développement agile (itérations courtes, revues de code régulières et validation progressive des fonctionnalités) ont été utilisés.
+
+# Organisation du travail en équipe, responsabilités et collaborations
+Placé sous la responsabilité de mes maîtres de stage, j'ai évolué au sein d'une équipe à taille humaine.
+
+Pour le développement, j'ai majoritairement collaboré avec une autre stagiaire présente en même temps que moi, ce qui nous a demandé de synchroniser nos tâches sur le code.
+Les retours et validations de code se faisaient de manière hybride. Nous utilisions des outils de messagerie instantanée (Teams et Discord) pour les points rapides du quotidien, 
+complétés par des points en face-à-face avec notre mentor pour valider nos réalisations avant de passer aux étapes suivantes.
+
+# Curiosité, créativité & autonomie
+Bien que j'ai pu réaliser plusieurs projets full-stack au cours de mes deux premières années de bachelor informatique à Ynov, cette immersion m'a poussé à faire preuve d'autonomie et de créativité, du au fait notamment de devoir s'approprier en un 
+temps relativement court un nouveau framework (Nuxt/TypeScript) que je n'avais pas encore exploré en profondeur en cours, en effectuant des recherches personnelles et de la veille technique.
+Face à la conception des pages de l'application VinoDomia, j'ai su prendre des initiatives sur l'ergonomie des interfaces pour proposer un design plus moderne et intuitif aux viticulteurs, tout en respectant les contraintes du cahier des charges.
+
+# Bilan personnel
+## Ressenti de l'apprenant sur l'expérience acquise :
+Ce stage a été extrêmement enrichissant. Il m'a permis d'approfondir concrètement mes connaissances en développement full-stack au sein d'un environnement professionnel réel, 
+confirmant l'intérêt que je porte à la conception de logiciels robustes.
+
+## Progression professionnelle à l'issue du stage :
+Au-delà de la montée en compétences techniques, cette expérience m'a permis de mûrir dans ma posture professionnelle. J'ai gagné en rigueur, notamment sur la propreté du code, la gestion des livrables et la communication au sein d'une équipe de développement.
+
+## Présentation d'une difficulté (forces et faiblesses) :
+La nécessité de maîtriser un nouveau framework tout en produisant immédiatement un code de qualité professionnelle, propre et maintenable, a représenté un défi complexe. Cette épreuve a néanmoins mis en avant ma capacité d'adaptation et ma persévérance. 
+Elle a en revanche révélé un besoin initial d'encadrement plus rapproché lors de la prise en main d'outils inconnus, une faiblesse que j'ai su compenser grâce à mon autonomie de recherche et aux retours de mes maîtres de stage.
+
+# Impact du stage sur le projet professionnel 
+Bien que le développement web ne constitue pas nécessairement ma spécialité prioritaire pour la suite de ma carrière, ce stage a eu un impact majeur sur mon projet professionnel. 
+Il m'a permis d'avoir un aperçu réaliste des méthodes de travail en milieu professionnel et de la rigueur exigée en entreprise, 
+et m'a doté d'une polyvalence technique indispensable pour aborder sereinement mes futures collaborations dans le secteur informatique.
+`}
+            showExpandBtn={true}/>
     ]
     const educationPath = [
         <StepComponent title={"Baccalauréat Général"} company={"Lycée Ozenne"} city={"Toulouse"} country={"France"}
@@ -75,8 +79,14 @@ export function AboutSection() {
 
     return (
         <div className={"w-full min-h-full flex flex-col items-center justify-start xl:px-80 py-20 gap-20"}>
+            <div>
+                <h2 className={"text-5xl md:text-6xl font-black mb-4 font-[Anybody] text-center text-black dark:text-white"}>À Propos</h2>
+                <p className={"w-full p-10 dark:bg-[#09090B]/45 bg-gray-200/45 text-lg text-center text-black dark:text-gray-300 rounded-3xl flex flex-col items-center justify-start py-10 px-15 gap-8 border border-white/30 dark:border-dark/30 hover:border-white dark:hover:border-black hover:bg-gray-200 dark:hover:bg-[#09090B]/70 transition-all duration-300"}>
+                    Bonjour et bienvenue sur mon portfolio ! Je m’appelle Noah CHARRIN-BOURRAT et je vais entrer en 3ème année de développement logiciel au Campus Ynov de Toulouse, en France. Au cours de ma première année, j’ai découvert un vif intérêt pour le développement d’applications mobiles, mais ma curiosité et ma passion pour l’informatique me poussent à explorer de nombreux domaines différents. J’aime réaliser des projets très variés, allant des jeux vidéo et des API aux applications web complètes. Je suis toujours impatient d’apprendre de nouvelles technologies, de relever des défis techniques et de transformer des idées en produits concrets et fonctionnels. Si mon profil vous intéresse, n’hésitez pas à me contacter via le formulaire de contact situé à la fin de ce portfolio.
+                </p>
+            </div>
             <div className={"flex flex-col items-center justify-center"}>
-                <h2 className={"text-6xl font-black mb-4 font-[Anybody] text-center"}>Experiences Professionnelle</h2>
+                <h2 className={"text-5xl md:text-6xl font-black mb-4 font-[Anybody] text-center text-black dark:text-white"}>Experiences Professionnelle</h2>
                 {carreerPath.map((val, index) =>
                     <React.Fragment key={`carrer-${index}`}>
                         {index == 0 &&
@@ -94,7 +104,7 @@ export function AboutSection() {
             </div>
 
             <div className={"flex flex-col items-center justify-center"}>
-                <h2 className={"text-6xl font-black mb-4 font-[Anybody]"}>Formations</h2>
+                <h2 className={"text-6xl font-black mb-4 font-[Anybody] text-black dark:text-white"}>Formations</h2>
                 {educationPath.map((val, index) =>
                     <React.Fragment key={`education-${index}`}>
                         {index == 0 &&
