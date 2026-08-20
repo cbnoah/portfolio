97d4e8ba86a9927ac9e981ef20126efb89f02263
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-08-21 01:10:53 +0200

    feat(App): update text color for improved readability in dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -69,31 +69,31 @@ function App() {
                 </div>
                 <div className={"flex flex-col gap-70"}>
                     <div
-                        className="w-full h-screen z-10 relative cursor-default select-none p-10 flex flex-col justify-end items-start gap-4"
+                        className="w-full h-screen z-10 relative cursor-default select-none p-5 xl:p-10 flex flex-col justify-end items-start gap-4"
                         id={"home"}>
                         <div className={"flex flex-row"}>
-                            <h1 className="text-black dark:text-white font-[Panchang-Variable] font-semibold text-7xl md:text-8xl xl:text-9xl hover:font-black transition-all">N</h1>
-                            <h1 className="text-black dark:text-white font-[Panchang-Variable] font-semibold text-7xl md:text-8xl xl:text-9xl hover:font-black transition-all">o</h1>
-                            <h1 className="text-black dark:text-white font-[Panchang-Variable] font-semibold text-7xl md:text-8xl xl:text-9xl hover:font-black transition-all">a</h1>
-                            <h1 className="text-black dark:text-white font-[Panchang-Variable] font-semibold text-7xl md:text-8xl xl:text-9xl hover:font-black transition-all">h</h1>
+                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-7xl md:text-8xl xl:text-9xl hover:font-black transition-all">N</h1>
+                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-7xl md:text-8xl xl:text-9xl hover:font-black transition-all">o</h1>
+                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-7xl md:text-8xl xl:text-9xl hover:font-black transition-all">a</h1>
+                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-7xl md:text-8xl xl:text-9xl hover:font-black transition-all">h</h1>
                         </div>
                         {/*name for big screens*/}
                         <div className="hidden flex-row justify-start items-center 2xl:flex">
-                            <h1 className="text-black dark:text-white font-[Panchang-Variable] font-semibold text-[6.3rem] hover:font-black transition-all duration-300">CHARRIN</h1>
+                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-[6.3rem] hover:font-black transition-all duration-300">CHARRIN</h1>
                             <div className={"flex flex-row"}>
-                                <h1 className="text-black dark:text-white font-[Panchang-Variable] font-semibold text-8xl">-</h1>
-                                <h1 className="text-black dark:text-white font-[Panchang-Variable] font-semibold text-8xl">-</h1>
+                                <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-8xl">-</h1>
+                                <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-8xl">-</h1>
                             </div>
-                            <h1 className="text-black dark:text-white font-[Panchang-Variable] font-semibold text-[6.3rem] hover:font-black transition-all duration-300">BOURRAT</h1>
+                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-[6.3rem] hover:font-black transition-all duration-300">BOURRAT</h1>
                         </div>
                         {/*name for smaller screens*/}
                         <div className="flex flex-col justify-center items-start 2xl:hidden">
-                            <h1 className="text-black dark:text-white font-[Panchang-Variable] font-semibold text-7xl md:text-8xl xl:text-[6.5rem] hover:font-black transition-all duration-300">CHARRIN--</h1>
-                            <h1 className="text-black dark:text-white font-[Panchang-Variable] font-semibold text-7xl md:text-8xl xl:text-[6.5rem] hover:font-black transition-all duration-300">BOURRAT</h1>
+                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-6xl md:text-8xl xl:text-[6.5rem] hover:font-black transition-all duration-300">CHARRIN--</h1>
+                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-6xl md:text-8xl xl:text-[6.5rem] hover:font-black transition-all duration-300">BOURRAT</h1>
                         </div>
                         <div
                             className={"w-full h-50 2xl:h-30 flex 2xl:flex-row flex-col 2xl:justify-between 2xl:items-center items-start gap-4"}>
-                            <h2 className="text-black dark:text-white font-[DINdong] font-black text-6xl xl:text-[4rem]">Etudiant en
+                            <h2 className="text-black dark:text-gray-100 font-[DINdong] font-black text-6xl xl:text-[4rem]">Etudiant en
                                 informatique</h2>
                             <div className={"xl:w-auto w-full flex flex-row xl:justify-center items-center gap-5"}>
                                 <ComicButton text="CV"
