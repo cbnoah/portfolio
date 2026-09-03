fa3a9259ad5b3cfe9f28d03451d9d525dc644b33
cnoah <noah.charrinbourrat@ynov.com>
2026-09-03 22:01:07 +0200

    feat(Background): update color gradient for dark theme to enhance visual appeal

--- src/component/Background.tsx
+++ src/component/Background.tsx
@@ -4,7 +4,9 @@ import {Color4Bg} from "@color4bg/react";
 export function Background() {
     const {theme} = useTheme();
     return <Color4Bg style={"curve-gradient"}
-                     colors={theme === "dark" ? ["#FDFDFD", "#DDDDDD", "#BBBBBB", "#555555", "#343434", "#010101"] : ["#5c5c5c", "#808080", "#b0b0b0", "#e6e6e6", "#EAEBE8", "#8f8f8f"]}
+                     colors={theme === "dark" ?
+                         ["#dcdcdc", "#b3b3b3", "#8f8f8f", "#353535", "#343434", "#010101"] :
+                         ["#5c5c5c", "#808080", "#b0b0b0", "#e6e6e6", "#EAEBE8", "#8f8f8f"]}
                      loop={true}
                      seed={1000}
                      options={{speed: 1, noise: 0.15}}/>;
