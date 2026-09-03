52c9596a6d19617bfa5c28ed2ee8217d6d02567d
cnoah <noah.charrinbourrat@ynov.com>
2026-09-03 22:04:15 +0200

    feat(ContactSection): enhance layout and styling for improved user engagement

--- src/component/sections/ContactSection.tsx
+++ src/component/sections/ContactSection.tsx
@@ -2,13 +2,13 @@ export function ContactSection() {
     return (
         <div className={"w-full h-full flex flex-col items-center justify-center py-20 p-20"}>
             <h2 className={"text-6xl font-black mb-4 font-[Anybody] text-center text-black dark:text-white"}>Contact</h2>
-            <div className={"w-full max-w-4xl h-full flex flex-col items-center justify-center p-8"}>
+            <div className={"w-full max-w-4xl h-full flex flex-col items-center justify-center p-8 gap-4"}>
                 <p className={"text-xl md:text-xl font-normal mb-4 font-[Anybody] text-center text-black dark:text-white"}>N'hésitez
                     pas à me contacter pour toute
                     question ou opportunité</p>
                 <form action="https://formsubmit.co/contact@cbnoah.com" method="POST" className={"flex flex-col gap-5"}>
                     <div
-                        className={"dark:bg-[#09090B]/45 bg-gray-200/45 md:w-175 xl:w-200 rounded-3xl flex flex-col items-center justify-start py-10 px-8 xl:px-15 pb-10 gap-8 border border-white/30"}>
+                        className={"dark:bg-[#09090B]/45 bg-gray-200/45 w-125 md:w-175 xl:w-200 rounded-3xl flex flex-col items-center justify-start py-10 px-8 xl:px-15 pb-10 gap-8 border border-white/30"}>
                         <div className={"flex flex-col w-full"}>
                             <label htmlFor={"name"} className={"jetbrains-mono text-black dark:text-white"}>Votre
                                 nom</label>
@@ -39,13 +39,13 @@ export function ContactSection() {
                         Envoyer
                     </button>
                 </form>
-                <div className={"flex flex-row gap-1.5 items-center justify-center"}>
-                    <p className={"text-black dark:text-white"}>Sinon envoyer moi un mail à
+                <div className={"w-125 md:w-175 xl:w-200 p-10 dark:bg-[#09090B]/45 bg-gray-200/45 text-lg text-center text-black dark:text-gray-300 rounded-3xl items-center justify-start py-10 px-15 gap-8 border border-white/30 dark:border-dark/30 hover:border-white dark:hover:border-black hover:bg-gray-200 dark:hover:bg-[#09090B]/70 transition-all duration-300"}>
+                    <p className={"text-black dark:text-white"}>Vous pouvez également me contacter par email à
                         <a href="mailto:contact@cbnoah.com"
                            className={"text-blue-500 hover:underline"}> contact@cbnoah.com </a>
                         ou envoyez moi un message sur <a
                             href="https://www.linkedin.com/in/noah-charrin-bourrat-9b3669332/"
-                            className={"text-blue-500 hover:underline"}>LinkedIn</a>
+                            className={"text-blue-500 hover:underline"}>LinkedIn </a> je vous répondrai dans les plus brefs délais.
                     </p>
                 </div>
             </div>
