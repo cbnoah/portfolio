0cbaf2237ddd58d5e9318efa1c7510bd62d1db96
cnoah <noah.charrinbourrat@ynov.com>
2026-09-05 02:11:42 +0200

    feat(ThemeToggle): replace icons with MorphIcon for improved theme toggle functionality

--- src/component/ThemeToggle.tsx
+++ src/component/ThemeToggle.tsx
@@ -1,18 +1,22 @@
 import {useTheme} from "../hooks/useTheme.ts";
-import {Moon, Sun} from "lucide-react";
+import {MorphIcon} from "morphicons/react";
+import {Moon, Sun} from "lucide";
 
 export function ThemeToggle() {
     const {theme, setTheme} = useTheme();
 
-
     return (
-        <button className={"cursor-pointer"} onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-            {theme === "dark" ?
-                <Sun size={30} strokeWidth={1.5}
-                     className={"hover:rotate-150 hover:scale-105 transition-transform duration-400"}/> :
-                <Moon size={30} strokeWidth={1.5}
-                      className={"hover:-rotate-360 hover:scale-105 transition-transform duration-400"}/>
-            }
+        <button
+            className={"p-3 rounded-3xl transition-all duration-300 hover:shadow-(--inset-top-bar-button-shadow) active:scale-95 cursor-pointer flex items-center justify-center text-black dark:text-gray-200"}
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            aria-label="Toggle theme">
+            <MorphIcon
+                className={"hover:rotate-360 hover:scale-105 transition-transform duration-400"}
+                strokeWidth={1.25}
+                spring={"snappy"}
+                size={30}
+                icon={theme === "dark" ? Sun : Moon}
+            />
         </button>
-    )
+    );
 }
