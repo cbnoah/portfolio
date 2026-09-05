dd8170564a333d7c9ee44f535b2a97be2788fb63
cnoah <noah.charrinbourrat@ynov.com>
2026-09-05 02:12:00 +0200

    feat(ProjectComponent): enhance layout and responsiveness, adjust typography and icon sizes

--- src/component/ProjectComponent.tsx
+++ src/component/ProjectComponent.tsx
@@ -47,11 +47,11 @@ export function ProjectComponent({
     let [height] = useState(showExpandBtn == true && showExpandBtn !== null ? "h-160" : "h-150");
     return (
         <div
-            className={`dark:bg-[#09090B]/45 bg-gray-200/45 ${description.length > 1000 ? (expanded ? "h-full" : height) : "h-full"} w-full rounded-3xl flex flex-col items-center justify-start py-10 px-15 gap-8 border border-white/30 dark:border-dark/30 hover:border-white dark:hover:border-black hover:bg-gray-200 dark:hover:bg-[#09090B]/70 transition-all duration-300`}>
+            className={`dark:bg-[#09090B]/45 bg-gray-200/45 ${description.length > 1000 ? (expanded ? "h-full" : height) : "h-full"} w-[80%] sm:w-[85%] md:w-[90%] xl:w-full mx-auto rounded-3xl flex flex-col items-center justify-start py-8 sm:py-10 px-5 sm:px-15 gap-8 border border-white/30 dark:border-dark/30 hover:border-white dark:hover:border-black hover:bg-gray-200 dark:hover:bg-[#09090B]/70 transition-all duration-300`}>
             {/*Component header (for infos)*/}
             <div className={"w-full h-full flex flex-col gap-4"}>
                 <div className={"w-full h-full flex flex-row items-start gap-6"}>
-                    <div className={"w-30 h-30 border border-white/30 dark:border-dark/30 rounded-2xl"}>
+                    <div className={"block w-30 h-30 border border-white/30 dark:border-dark/30 rounded-2xl shrink-0"}>
                         {projectImagePath === undefined || projectImagePath === "" ?
                             <span
                                 className={"w-full h-full flex text-2xl text-center justify-center items-center text-black dark:text-white"}>Aucune Image</span> :
@@ -61,27 +61,27 @@ export function ProjectComponent({
                                  loading={"lazy"}/>
                         }
                     </div>
-                    <div className={"flex flex-col justify-between items-start"}>
-                        <h3 className={"text-4xl font-extrabold mb-2 font-[Anybody] text-black dark:text-white"}>{title}</h3>
+                    <div className={"flex flex-col justify-between items-start min-w-0"}>
+                        <h3 className={"text-2xl sm:text-4xl font-extrabold mb-2 font-[Anybody] text-black dark:text-white wrap-anywhere"}>{title}</h3>
                         <div className={"flex flex-row gap-2 text-black dark:text-white"}>
-                            {group ? <UsersRound/> : <UserRound/>}
-                            <h4 className={"text-lg mb-2 jetbrains-mono text-black dark:text-white"}>{group ? "En Groupe" : "Seul"}</h4>
+                            {group ? <UsersRound className={"size-4.5 sm:size-6"}/> : <UserRound className={"size-4.5 sm:size-6"}/>}
+                            <h4 className={"text-sm sm:text-lg mb-2 jetbrains-mono text-black dark:text-white"}>{group ? "En Groupe" : "Seul"}</h4>
                         </div>
                         <div className={"flex flex-row gap-2 text-black dark:text-white"}>
-                            {finished ? <Check/> : <Clock/>}
-                            <h4 className={"text-lg mb-2 jetbrains-mono text-black dark:text-white"}>{finished ? "Projet Fini" : "En Cours"}</h4>
+                            {finished ? <Check className={"size-4.5 sm:size-6"}/> : <Clock className={"size-4.5 sm:size-6"}/>}
+                            <h4 className={"text-sm sm:text-lg mb-2 jetbrains-mono text-black dark:text-white"}>{finished ? "Projet Fini" : "En Cours"}</h4>
                         </div>
                     </div>
                 </div>
                 <div
-                    className={"w-fit h-full flex flex-row items-center justify-start gap-4"}>{(tags !== null && tags !== undefined && tags.length > 0) && tags.map((tag, index) => {
+                    className={"w-full flex flex-wrap items-center justify-start gap-2 sm:gap-4"}>{(tags !== null && tags !== undefined && tags.length > 0) && tags.map((tag, index) => {
                     return (
                         <div
-                            className={`w-32 flex flex-row items-center justify-start gap-2 px-2 py-1 text-md font-medium jetbrains-mono text-left dark:bg-[#09090B]/50 bg-gray-100/80 rounded-xl border text-black dark:text-white`}
+                            className={`w-25 sm:w-32 flex flex-row items-center justify-start gap-2 px-2 py-1 text-sm sm:text-md font-medium jetbrains-mono text-left dark:bg-[#09090B]/50 bg-gray-100/80 rounded-xl border text-black dark:text-white`}
                             key={index}
                             style={{borderColor: tag.color}}>
                             <div className="shrink-0 flex items-center">
-                                <tag.icon/>
+                                <tag.icon className={"size-4.5 sm:size-6"}/>
                             </div>
                             <span className="truncate">
                                 {tag.title}
@@ -111,7 +111,7 @@ export function ProjectComponent({
                     {expanded ? <Minimize2 strokeWidth={1.5} size={19}/> : <Maximize2 strokeWidth={1.5} size={19}/>}
                 </button>
             }</>
-            <div className={"w-full flex flex-row gap-4"}>
+            <div className={"w-full flex flex-col md:flex-row gap-4"}>
                 {(links !== undefined && links!.length > 0) && links?.map((link, index) => {
                     return (
                         <a className={"w-full flex flex-row gap-3 p-5 items-center justify-center jetbrains-mono font-bold bg-gray-100/60 " +
