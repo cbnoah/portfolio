3684b0e894cbc8691c629be9fef283c2a003d5fc
cnoah <noah.charrinbourrat@ynov.com>
2026-09-05 02:12:36 +0200

    feat(App): enhance text sizes for improved responsiveness and update links for accessibility

--- src/App.tsx
+++ src/App.tsx
@@ -68,38 +68,33 @@ function App() {
                 <div className="fixed inset-0 z-0 w-screen h-screen pointer-events-none">
                     <Background/>
                 </div>
-                <div className={"flex flex-col gap-70"}>
+                <div className={"flex flex-col gap-70 md:px-0"}>
                     <div
-                        className="w-full h-screen z-10 relative cursor-default select-none p-5 xl:p-10 flex flex-col justify-end items-start gap-4"
+                        className="w-full h-screen z-10 relative cursor-default select-none px-5 pt-5 pb-20 xl:p-10 flex flex-col justify-end items-start gap-4"
                         id={"home"}>
                         <div className={"flex flex-row"}>
-                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-7xl md:text-8xl xl:text-9xl hover:font-black transition-all">N</h1>
-                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-7xl md:text-8xl xl:text-9xl hover:font-black transition-all">o</h1>
-                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-7xl md:text-8xl xl:text-9xl hover:font-black transition-all">a</h1>
-                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-7xl md:text-8xl xl:text-9xl hover:font-black transition-all">h</h1>
+                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-5xl sm:text-7xl md:text-9xl xl:text-9xl hover:font-black transition-all">N</h1>
+                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-5xl sm:text-7xl md:text-9xl xl:text-9xl hover:font-black transition-all">o</h1>
+                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-5xl sm:text-7xl md:text-9xl xl:text-9xl hover:font-black transition-all">a</h1>
+                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-5xl sm:text-7xl md:text-9xl xl:text-9xl hover:font-black transition-all">h</h1>
                         </div>
                         {/*name for big screens*/}
-                        <div className="hidden flex-row justify-start items-center 2xl:flex">
-                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-[6.3rem] hover:font-black transition-all duration-300">CHARRIN</h1>
+                        <div className="2xl:flex-row flex-col justify-start items-center 2xl:flex">
+                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-5xl sm:text-7xl md:text-8xl xl:text-[6.3rem] hover:font-black transition-all duration-300">CHARRIN</h1>
                             <div className={"flex flex-row"}>
-                                <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-8xl">-</h1>
-                                <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-8xl">-</h1>
+                                <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-5xl sm:text-6xl md:text-9xl xl:text-9xl hover:font-black transition-all">-</h1>
+                                <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-5xl sm:text-6xl md:text-9xl xl:text-9xl hover:font-black transition-all">-</h1>
                             </div>
-                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-[6.3rem] hover:font-black transition-all duration-300">BOURRAT</h1>
-                        </div>
-                        {/*name for smaller screens*/}
-                        <div className="flex flex-col justify-center items-start 2xl:hidden">
-                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-6xl md:text-8xl xl:text-[6.5rem] hover:font-black transition-all duration-300">CHARRIN--</h1>
-                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-6xl md:text-8xl xl:text-[6.5rem] hover:font-black transition-all duration-300">BOURRAT</h1>
+                            <h1 className="text-black dark:text-gray-100 font-[Panchang-Variable] font-semibold text-5xl sm:text-7xl md:text-8xl xl:text-[6.3rem] hover:font-black transition-all duration-300">BOURRAT</h1>
                         </div>
                         <div
                             className={"w-full h-50 2xl:h-30 flex 2xl:flex-row flex-col 2xl:justify-between 2xl:items-center items-start gap-4"}>
-                            <h2 className="text-black dark:text-gray-100 font-[DINdong] font-black text-6xl xl:text-[4rem]">Etudiant en
+                            <h2 className="text-black dark:text-gray-100 font-[DINdong] font-black text-4xl sm:text-5xl md:text-6xl xl:text-[4rem] pt-5 xl:pt-0">Etudiant en
                                 informatique</h2>
                             <div className={"xl:w-auto w-full flex flex-row xl:justify-center items-center gap-5"}>
                                 <ComicButton text="CV"
-                                             link="https://drive.google.com/file/d/1G76TPsTcdBMbECQ9myqVTopUqWO0IgC-/view?usp=sharing"/>
-                                <ComicButton text="GitHub" link="https://github.com/cbnoah"/>
+                                             link="https://drive.google.com/file/d/1rTfKRrt-SmrfGUq-6-Bnib_ynqFG4z5g/view?usp=sharing"/>
+                                <ComicButton text="GitHub" link="https://github.cbnoah.com"/>
                             </div>
                         </div>
                     </div>
